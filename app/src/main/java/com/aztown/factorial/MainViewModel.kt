b74eb2dd4234dd8d917b53a00636fa00f67c4cf7
azmetoff <azmetoff@gmail.com>
2022-05-12 17:44:48 +0300

    experiments with coroutineContext

--- app/src/main/java/com/aztown/factorial/MainViewModel.kt
+++ app/src/main/java/com/aztown/factorial/MainViewModel.kt
@@ -1,15 +1,18 @@
 package com.aztown.factorial
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel : ViewModel() {
+    val coroutineScope = CoroutineScope(Dispatchers.Main + CoroutineName("My coroutine scope"))
+
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
@@ -20,18 +23,32 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
-        viewModelScope.launch{
+        coroutineScope.launch {
             val result = factorial(value.toLong())
             _state.value = Factorial(result)
+            Log.d("MainViewModel", coroutineContext.toString())
         }
     }
 
-    private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number)
-                result = result.multiply(BigInteger.valueOf(i))
-            result.toString()
-        }
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number)
+            result = result.multiply(BigInteger.valueOf(i))
+        return result.toString()
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+//    private suspend fun factorial(number: Long): String {
+//        return suspendCoroutine {
+//            thread {
+//                var result = BigInteger.ONE
+//                for (i in 1..number)
+//                    result = result.multiply(BigInteger.valueOf(i))
+//                it.resumeWith(Result.success(result.toString()))
+//            }
+//        }
+//    }
 }
