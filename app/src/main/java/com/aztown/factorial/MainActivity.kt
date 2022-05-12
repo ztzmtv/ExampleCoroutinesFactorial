c4f4db4a3e255df8cc1d65c90e9347173f0b9318
azmetoff <azmetoff@gmail.com>
2022-05-12 11:44:16 +0300

    create viewModel and observe it

--- app/src/main/java/com/aztown/factorial/MainActivity.kt
+++ app/src/main/java/com/aztown/factorial/MainActivity.kt
@@ -2,10 +2,50 @@ package com.aztown.factorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.aztown.factorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.progress.observe(this) {
+            if (it) {
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+        viewModel.error.observe(this) {
+            if (it) {
+                Toast.makeText(
+                    this, "You didn't enter the value", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        viewModel.factorial.observe(this) {
+            binding.textViewFactorial.text = it
+        }
     }
 }
